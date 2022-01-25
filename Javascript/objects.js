c7be3856cd55f689e490e87637a68789db7421ca
Crosshair <bertold95pk@gmail.com>
2022-01-25 21:14:39 +0100

    v 1.0

---
+++ Javascript/objects.js
@@ -0,0 +1,96 @@
+/*  ↓  Alaplap  ↓  */
+const motherboard = {
+    Name = "",
+    //Capacity = ,
+    RamSlot = "",   //[DDR3 , DDR4]
+    Brand = "",     //[Intel , AMD]
+    Slots = 0       //
+    CPUSlot =       //[AM4 , LGA1151 , LGA1200]
+    StorageSlot = 0 //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+    Case = "",      //[ATX Full , ATX Mid , ATX Mini]
+};
+/*  ↑  Alaplap  ↑  */
+
+
+/*  ↓  Processor  ↓  */
+const cpu = {
+    Name = "",      
+    Capacity = 0,   //Ide írhatjuk a processzor órajelét
+    //RamSlot = "", //[DDR3 , DDR4]
+    Brand = "",     //[Intel , AMD]
+    //Slots = 0     
+    CPUSlot = ""    //[AM4 , LGA1151 , LGA1200]
+    //StorageSlot = //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+    Case = "",      //[ATX Full , ATX Mid , ATX Mini]
+};
+/*  ↑  Processor  ↑  */
+
+
+/*  ↓  RAM  ↓  */
+const ram = {
+    Name = "",
+    Capacity = 0,   //[4Gb , 8Gb]
+    RamSlot = "",   //[DDR3 , DDR4]
+    //Brand = "",   //--Ezt később mert nem fontos--
+    //Slots = 0       
+    //CPUSlot = ""  //[AM4 , LGA1151 , LGA1200]
+    //StorageSlot = //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+    Case = "",      //[ATX Full , ATX Mid , ATX Mini]
+};
+/*  ↑  RAM  ↑  */
+
+
+/*  ↓  Storage  ↓  */
+const storage = {
+    Name = "",
+    Capacity = 0,       //[500Gb , 1Tb , 2Tb]
+    //RamSlot = "",
+    //Brand = "",      //--opcionális--
+    //Slots = 0
+    //CPUSlot = ""
+    StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+    Case = "",      //[ATX Full , ATX Mid , ATX Mini]
+};
+/*  ↑  Storage  ↑  */
+
+
+/*  ↓  StorageHDD  ↓  */
+const hdd = {
+    Name = "",
+    Capacity = 0,        //[500Gb , 1Tb , 2Tb]
+    //RamSlot = "",
+    //Brand = "",       //--opcionális--
+    //Slots = 0
+    //CPUSlot = ""
+    StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+    Case = "",      //[ATX Full , ATX Mid , ATX Mini]
+};
+/*  ↑  StorageHDD  ↑  */
+
+
+/*  ↓  StorageSSD  ↓  */
+const ssd = {
+    Name = "",          
+    Capacity = 0,        //[500Gb , 1Tb , 2Tb]
+    //RamSlot = "",
+    //Brand = "",       //--opcionális--
+    //Slots = 0
+    //CPUSlot = ""
+    StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+    Case = "",      //[ATX Full , ATX Mid , ATX Mini]
+};
+/*  ↑  StorageSSD  ↑  */
+
+
+/*  ↓  Power Supply  ↓  */
+const pwrsply = {
+    Name = "",
+    Capacity = 0,        //[500W , 600W , 700W]
+    //RamSlot = "",
+    //Brand = "",
+    //Slots = 0
+    //CPUSlot = ""
+    //StorageSlot =     //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+    Case = "",          //[ATX Full , ATX Mid , ATX Mini]
+};
+/*  ↑  Power Supply  ↑  */
