c802a9e33f47eb1823a75205f5d79a852943ebfc
Crosshair <bertold95pk@gmail.com>
2022-02-08 22:56:30 +0100

    v1.0.9

--- Javascript/pluszminus.js
+++ Javascript/pluszminus.js
@@ -1,22 +1,22 @@
 jQuery(function(){
     var j = jQuery; // jQuery variable
-    var addInput = '#qty'; //This is the id of the input you are changing
+    var addInput = '#qty'; // Ez az Input mező ID-ja amit meg fogunk változtatni
     var n = 1;
     
     // Az N értékét beállítjuk 1-re
     j(addInput).val(n);
   
     // Hozzáadunk az N-hez 1-et
-    j('.plus').on('click', function(){
+    j('.plusbutton').on('click', function(){
       j(addInput).val(++n);
     })
   
-    j('.min').on('click', function(){
+    j('.minbutton').on('click', function(){
       // Ha az N több mint 1 akkor kivon 1-et
       if (n >= 1) {
         j(addInput).val(--n);
       } else {
-        // Ellenkező esetben ne csináljon semmit
+      // Ellenkező esetben ne csináljon semmit
       }
     });
   });
---
+++ ReadMe.txt
@@ -0,0 +1,40 @@
+//==============================//
+//	Bertold kommentjei	//
+//==============================//
+|
+|1,
+|Ez még mindig csak egy DEMO. Többet nem izélek a formájával, rakom bele a scripteket.
+|Ehhez még ne nyúlj amíg nem frissítem ezt a leírást is mert még mászkálok a fájlokban.
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+//==============================//
+//	Bertold kommentjei	//
+//==============================//
