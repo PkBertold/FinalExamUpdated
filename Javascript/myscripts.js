63fb5f8272f8117b5bbd3e2321f248a65a34dc46
Crosshair <bertold95pk@gmail.com>
2022-03-25 19:37:40 +0100

    refresh

---
+++ Javascript/myscripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".btnmotherboard").click(function(){
+      $(".motherboard").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".btncpu").click(function(){
+      $(".cpu").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".btnram").click(function(){
+      $(".ram").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".btngpu").click(function(){
+      $(".gpu").toggle();
+    });
+  });
